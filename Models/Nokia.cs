774b56bf7e3e160626d56bc0b2968d70a0e735d6
Gustavo Matos <gustavo.matosdonascimento@outlook.com>
2023-11-12 09:43:07 -0300

    Desafio concluído!

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,16 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // TODO: Herdar da classe "Smartphone" - OK
+    public class Nokia : Smartphone
     {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} através da Play Store");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+// Teste smartphone Nokia
+
+Smartphone nokia = new Nokia(numero: "981100", modelo: "Tijolão", imei: "443334513", memoria: 1);
+nokia.Ligar();
+nokia.InstalarAplicativo("Agenda");
+
+// Teste smartphone Iphone
+
+Smartphone iphone = new Iphone(numero: "984422", modelo: "iPhone 15", imei: "89777632", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("WhatsApp");
